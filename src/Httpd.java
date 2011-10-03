47bfd987fcf3b5f3666bce88a4c99535b78abbfb
Miguel Velez <esumike22@gmail.com>
2011-10-02 23:17:16 -0700

    Creacion de carpeta resources, que contiene todos los archivos.html

--- src/Httpd.java
+++ src/Httpd.java
@@ -14,20 +14,29 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Httpd {
-    public Httpd (int port) {
+public class Httpd 
+{
+    public Httpd (int port) 
+    {
         ServerSocket s = null;
-        try {
+        try 
+        {
             s = new ServerSocket(port);
-        } catch(IOException ioe) {
+        } 
+        catch(IOException ioe) 
+        {
             System.err.println("ServerSocket()");
             System.exit(1);
         }
-        while (true) {
+        while (true) 
+        {
             Socket c = null;
-            try {
+            try 
+            {
                 c = s.accept();
-            } catch(IOException ioe) {
+            } 
+            catch(IOException ioe) 
+            {
                 System.err.println("accept()");
                 System.exit(1);
             }
@@ -35,20 +44,27 @@ public class Httpd {
         }
     }
 
-    private class Handler extends Thread {
+    private class Handler extends Thread 
+    {
         private Map<String, String> _map;
         private Socket _c;
 
-        public Handler (Socket c) {
+        public Handler (Socket c) 
+        {
             _c = c;
-            _map = new HashMap<String, String>() {{
-                put("html", "text/html");
-                put("txt", "text/plain");
-            }};
+            _map = new HashMap<String, String>() 
+            {
+            	{
+            		put("html", "text/html");
+            		put("txt", "text/plain");
+            	}
+            };
         }
 
-        public void run () {
-            try {
+        public void run () 
+        {
+            try 
+            {
                 InputStream is = _c.getInputStream();
                 InputStreamReader isr = new InputStreamReader(is);
                 BufferedReader br = new BufferedReader(isr);
@@ -56,31 +72,37 @@ public class Httpd {
                 OutputStream os = _c.getOutputStream();
                 OutputStreamWriter osw = new OutputStreamWriter(os);
                 BufferedWriter bw = new BufferedWriter(osw);
-                if (3 == command.length) {
-                    if ("GET".equals(command[0])) {
+                if (3 == command.length) 
+                {
+                    if ("GET".equals(command[0])) 
+                    {
                         URL u = new URL("http", "localhost", 80, command[1]);
                         File f = new File(".", u.getPath());
                         String name = f.getName();
                         String ext = "";
                         String parts[] = name.split("\\.");
-                        if (1 < parts.length) {
+                        if (1 < parts.length) 
+                        {
                             ext = parts[parts.length - 1].toLowerCase();
                         }
-                        if (f.canRead()) {
+                        if (f.canRead()) 
+                        {
                             long bytes = 0;
                             FileReader fr;
                             BufferedReader bfr;
                             fr = new FileReader(f);
                             bfr = new BufferedReader(fr);
                             int c;
-                            while (-1 != (c=bfr.read())) {
+                            while (-1 != (c=bfr.read())) 
+                            {
                                 bytes += 1;
                             }
                             
                             fr = new FileReader(f);
                             bfr = new BufferedReader(fr);
                             String type = "text/plain"; // Default
-                            if (_map.containsKey(ext)) {
+                            if (_map.containsKey(ext)) 
+                            {
                                 type = _map.get(ext);
                             }
                             bw.write("HTTP/1.0 200 Ok");
@@ -92,28 +114,38 @@ public class Httpd {
                             bw.write(String.format("Content-Length: %d", bytes));
                             bw.newLine();
                             bw.newLine();
-                            while (-1 != (c=bfr.read())) {
+                            while (-1 != (c=bfr.read())) 
+                            {
                                 bw.write(c);
                             }
                             bw.flush();
                             bw.close();
                         }
                         //codigo de fofo
-                        else{
-                        	
+                        else
+                        {
+                        		
                         }
                     }
                 }
-            } catch(IOException ioe) {
+            } 
+            catch(IOException ioe) 
+            {
+            	
             }
-            try {
+            try 
+            {
                 _c.close();
-            } catch(IOException ioe) {
+            } 
+            catch(IOException ioe) 
+            {
+            	
             }
         }
     }
 
-    public static void main (String args[]) {
+    public static void main (String args[]) 
+    {
         new Httpd(80);
     }
 }
